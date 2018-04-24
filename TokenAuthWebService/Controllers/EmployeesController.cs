34fc4fcd74f1b4b4a6171a0aedc1d85621877a53
D. Gormash <dlgormash@gmail.com>
2018-04-24 11:45:30 +0500

    Добавлен контроллер Employees с методом Get, возвращающий список сотрудников из базы данных

---
+++ TokenAuthWebService/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeDataAccess;
+
+namespace TokenAuthWebService.Controllers
+{
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<EmployeeModel> Get()
+        {
+            var sqlite = new SQLiteWorker();
+
+            return sqlite.GetEmploees();
+        } 
+    }
+}
